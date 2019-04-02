1947e33b4d935e993e77076ae4a97dff0c8665ce
ZoeWang0505 <40203903+ZoeWang0505@users.noreply.github.com>
2019-04-02 14:36:03 -0400

    update code base

--- src/FlappyGhost/Jeu.java
+++ src/FlappyGhost/Jeu.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Jeu extends Application {
 
+    private int WIDTH = 480;
+    private int HEIGHT = 300;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FlappyGhost.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Pane root = new Pane();
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
