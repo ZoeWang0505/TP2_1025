7ecb21e25b856ff1e686306c21d9e122d31ad8c3
DavidRP1 <36389670+DavidRP1@users.noreply.github.com>
2019-04-10 14:17:30 -0400

    add commentaries

--- src/FlappyGhost/Vue.java
+++ src/FlappyGhost/Vue.java
@@ -1,8 +1,17 @@
-package FlappyGhost;
+/**
+ * Auteurs:     David Raby-Pepin, p0918119
+ *              Zoe Wang, p20111352
+ *
+ * Date:        22/04/2019
+ *
+ * Description: Ce programme est un jeu en interface graphique avec la librairie JavaFX.
+ *              Le joueur incarne un fantôme qui se promène dans un niveau rempli d’obstacles. Le seul objectif
+ *              est d’avancer le plus longtemps possible dans le niveau sans toucher d’obstacle.
+ */
 
+package FlappyGhost;
 import javafx.application.Application;
 import javafx.application.Platform;
-
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -10,48 +19,64 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
-
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.util.Iterator;
 
 public class Vue extends Application {
-    private Controleur controleur;  // Contrôleur de l'application
-    private int lrgFenetre = 640;
-    private int htrFenetre = 440;
-    private int htrBarreTache = 40;
-    private GraphicsContext context;
-    private Boolean modeDebug = false;
-    private String strPause = "Pause";
-    private String strResume = "Resume";
+    private Controleur controleur;       // Contrôleur de l'application
+    private int lrgFenetre = 640;        // largeur de la fenetre
+    private int htrFenetre = 440;        // hauteur de la fenetre
+    private int htrBarreTache = 40;      // hauteur de la barre de tache
+    private GraphicsContext context;     // context graphique du canva
+    private Boolean modeDebug = false;   // indique si le mode debug est active
+    private String strPause = "Pause";   // affichage sur le bouton pause
+    private String strResume = "Resume"; // affichage sur le bouton pause
+    Text score;                          // score du joueur a afficher
 
-    Text score;
-
-
+    /**
+     * Getter pour la largeur de la fenetre
+     * @return largeur de la fenetre
+     */
     public int getLrgFenetre() {
         return lrgFenetre;
     }
 
+    /**
+     * Getter pour la hauteur de la fenetre
+     * @return hauteur de la fenetre
+     */
     public int getHtrFenetre() {
         return htrFenetre;
     }
 
+    /**
+     * Getter pour la hauteur de la barre de tache
+     * @return hauteur de la barre de tache
+     */
     public int getHtrBarreTache() {
         return htrBarreTache;
     }
 
+    /**
+     * Getter pour l'etat du mode debug
+     * @return etat du mode debug
+     */
     public boolean getModeDebug(){
         return this.modeDebug;
     }
 
-
+    /**
+     * Fonction principale
+     * @param args arguments entres en ligne de commande
+     */
     public static void main(String[] args) {Vue.launch(args);}
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // creation de la vue initiale du jeu
         VBox root = new VBox(8);
         Scene scene = new Scene(root, lrgFenetre, htrFenetre);
 
@@ -73,8 +98,8 @@ public class Vue extends Application {
         primaryStage.setTitle("Flappy Ghost");
         primaryStage.getIcons().add(Resources.getGhost());
         primaryStage.show();
-
         context = canvas.getGraphicsContext2D();
+
         // Création du contrôleur
         controleur = new Controleur(this);
         controleur.start();
@@ -89,7 +114,6 @@ public class Vue extends Application {
             canvas.requestFocus();
         });
 
-
         // si l'utilisateur pese sur la barre d'espace, avertir le controleur
         scene.setOnKeyPressed((value) -> {
             switch (value.getCode()){
@@ -100,32 +124,38 @@ public class Vue extends Application {
                     primaryStage.close();
                     break;
             }
-
         });
 
+        // si le jour clique sur le bouton pause, mettre le jeu sur pause et changer le texte du bouton a resume
         pause.setOnAction((event)-> {
             this.controleur.evenement(Jeu.actions.PAUSE);
             String str = pause.getText() == strPause ? strResume : strPause;
             pause.setText(str);
         });
 
+        // si le joueur clique sur la case du mode debug, activer le mode debug
         modeDebug.setOnAction((event)-> {
             this.modeDebug = modeDebug.isSelected();
         });
 
     }
 
+    /**
+     * Mets a jour la vue du jeu selon les changements apportes au modele
+     * @param jeu modele
+     */
     public void miseAJour(Jeu jeu) {
-        //Afficher Background
+        // mettre a jour l'arriere-plan
         Background bg = jeu.getBackgroud();
         context.clearRect(0, 0, lrgFenetre, htrFenetre - htrBarreTache);
         Image image = bg.getImage();
         context.drawImage(image, bg.getCoordX(),0, image.getWidth(), image.getHeight() );
         context.drawImage(image, (bg.getCoordX() + lrgFenetre), 0,image.getWidth(), image.getHeight());
 
+        // mettre a jour le score dujoueur
         score.setText("Score: " + jeu.getScore());
 
-        // afficher le fantome
+        // mettre a jour le fantome
         Fantome fantome = jeu.getFantome();
         if(!modeDebug) {
             context.drawImage(fantome.getImage(), fantome.getCoordX() - fantome.getRayon(),
@@ -137,7 +167,7 @@ public class Vue extends Application {
                     fantome.getRayon() * 2, fantome.getRayon() * 2);
         }
 
-        // afficher les obstacles
+        // mettre a jour les obstacles
         Iterator it = jeu.getListeObstacles().iterator();
         while(it.hasNext()){
             Obstacle obstacle = (Obstacle) it.next();
@@ -147,11 +177,11 @@ public class Vue extends Application {
                         obstacle.getRayon() * 2, obstacle.getRayon() * 2);
             } else {
                 context.setFill(obstacle.getCouleur());
-                context.fillOval(obstacle.getCoordX()- obstacle.getRayon(), obstacle.getCoordY() - obstacle.getRayon(),
+                context.fillOval(obstacle.getCoordX()- obstacle.getRayon(),
+                        obstacle.getCoordY() - obstacle.getRayon(),
                         obstacle.getRayon() * 2, obstacle.getRayon() * 2);
             }
         }
-
     }
 }
 
